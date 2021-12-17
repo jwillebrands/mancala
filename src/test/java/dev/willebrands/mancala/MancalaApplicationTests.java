48d1082d49d4744f113f23ae7b4514ccdb1e540b
Jan-Willem Willebrands <jan-willem@willebrands.dev>
2021-12-17 18:08:54 +0100

    scaffold backend project using Spring Initializr

---
+++ src/test/java/dev/willebrands/mancala/MancalaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.willebrands.mancala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MancalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
